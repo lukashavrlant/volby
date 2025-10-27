5d8347b3ee2fdf438dc8fd8703831590a328c316
Lukáš Havrlant <lukas.havrlant@gmail.com>
2025-10-27 16:48:17 +0100

    mandaty pro kraje

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -2,25 +2,50 @@ import { join } from "path";
 
 const { readFileSync } = require('fs');
 const { parseStringPromise } = require('xml2js');
-// ... zbytek TypeScript kódu
-
-
-// Typ popisující jednu stranu
 interface Strana {
     nazev: string;
     hlasy: number;
 }
-
-// Typ popisující jeden kraj
 interface VysledekKraje {
     kraj: string;
     platneHlasy: number;
     strany: Strana[];
+    prideleneMandatyPrvniKolo?: number;
 }
+
+type VysledekRepubliky = ReadonlyArray<VysledekKraje>;
+
+const POCET_MANDATU = 200;
 
 async function run() {
     const results = await parseResults();
-    console.log(results);
+    const vysledekRepubliky = results.sort((k1, k2) => k1.platneHlasy - k2.platneHlasy);
+    const soucetHlasu = sectiHlasy(vysledekRepubliky);
+    const republikoveMandatoveCislo = Math.round(soucetHlasu / POCET_MANDATU);
+    const mandatyProKraje = vypoctiMandatyProKraje(vysledekRepubliky, republikoveMandatoveCislo);
+    const pocetNepridelenychMandatuKrajum = spocitejPocetNepridelenychMandatuKrajum(mandatyProKraje);
+
+    console.log(vysledekRepubliky);
+    console.log({soucetHlasu, republikoveMandatoveCislo});
+    console.log(mandatyProKraje);
+    console.log({pocetNepridelenychMandatuKrajum})
+}
+
+function spocitejPocetNepridelenychMandatuKrajum(vysledekRepubliky: VysledekRepubliky): number {
+    return POCET_MANDATU - vysledekRepubliky.reduce((p, c) => p + (c.prideleneMandatyPrvniKolo ?? 0), 0);
+}
+
+function vypoctiMandatyProKraje(vysledekRepubliky: VysledekRepubliky, republikoveMandatoveCislo: number) {
+    return vysledekRepubliky.map(kraj => {
+        return {
+            ...kraj,
+            prideleneMandatyPrvniKolo: Math.floor(kraj.platneHlasy / republikoveMandatoveCislo)
+        }
+    });
+}
+
+function sectiHlasy(vysledek: VysledekRepubliky): number {
+    return vysledek.reduce((p, c) => p + c.platneHlasy, 0);
 }
 
 async function parseResults(): Promise<VysledekKraje[]> {
